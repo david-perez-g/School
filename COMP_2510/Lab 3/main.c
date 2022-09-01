a0a5619f9f518adbc8910ee049db1cc90a5eb56c
David Pérez <david.perez.garcia.2508@gmail.com>
2022-08-31 21:16:18 -0400

    added COMP_2510/Lab 3

---
+++ COMP_2510/Lab 3/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void printTokens(char *inputString)
+{
+    bool word = false;
+
+    while (*inputString)
+    {
+        if (*inputString == ' ')
+        {
+            if (word)
+            {
+                // the word ended
+                printf("\n");
+                word = false;
+            }
+
+            inputString++;
+            continue;
+        }
+        word = true;
+        printf("%c", *inputString++);
+    }
+
+    printf("\n");
+}
+
+int main(void)
+{
+    printTokens("  Hello  this  is a test  ");
+}
