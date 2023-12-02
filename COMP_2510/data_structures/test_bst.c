9b6dd1566a5fc93eb90bc992afb4450d1d2585fa
David <david.perez.garcia.2508@gmail.com>
2023-12-02 12:04:08 -0500

    added tests for the new implementation of the bst iterator

--- COMP_2510/data_structures/test_bst.c
+++ COMP_2510/data_structures/test_bst.c
@@ -3,42 +3,40 @@
 
 #include "bst.h"
 
-int int_cmp(void* a, void* b) {
-    int int_a = *(int*)a;
-    int int_b = *(int*)b;
-
-    if (int_a == int_b) {
-        return 0;
-    }
-
-    if (int_a > int_b) {
-        return 1;
-    }
-
-    return -1;
+int cmp(void* a, void* b) {
+    int int_a = *((int*) a);
+    int int_b = *((int*) b);
+    if (int_a == int_b) return 0;
+    if (int_a < int_b) return -1;
+    return 1;
 }
 
 int main() {
-    bstree_t* tree = bst_create(int_cmp);
+    // Test 1: Inserting and finding values
+    bstree_t* tree = bst_create(cmp);
+    int value1 = 5;
+    int value2 = 10;
+    int value3 = 15;
+    bst_insert(tree, &value1);
+    bst_insert(tree, &value2);
+    bst_insert(tree, &value3);
+    assert(bst_find(tree, &value1)->value == &value1);
+    assert(bst_find(tree, &value2)->value == &value2);
+    assert(bst_find(tree, &value3)->value == &value3);
+    printf("Test 1 passed.\n");
 
-    int values[] = {5, 3, 7, 2, 4, 6, 8};
-    for (int i = 0; i < 7; i++) {
-        printf("Hola 0\n");
-        bst_insert(tree, &values[i]);
-    }
+    // Test 2: In-order iterator
+    bst_iterator_t* it = bst_get_inorder_iterator(tree);
+    assert(bst_iter_has_next(it));
+    assert(bst_iter_next(it) == &value1);
+    assert(bst_iter_has_next(it));
+    assert(bst_iter_next(it) == &value2);
+    assert(bst_iter_has_next(it));
+    assert(bst_iter_next(it) == &value3);
+    assert(!bst_iter_has_next(it));
+    bst_free_iterator(it);
+    printf("Test 2 passed.\n");
 
-    for (int i = 0; i < 7; i++) {
-        printf("Hola 1\n");
-        assert(bst_find(tree, &values[i]) != NULL);
-    }
-
-    int not_in_tree = 9;
-    printf("Hola 2\n");
-    assert(bst_find(tree, &not_in_tree) == NULL);
-
-    printf("All tests passed!\n");
     bst_free(tree, false);
-
     return 0;
 }
-
